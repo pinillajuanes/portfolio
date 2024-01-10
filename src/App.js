83c876646d0a0a11ad13577ea4f53ce77c070c13
Juanes Pinilla <juanpinpin@unisabana.edu.co>
2024-01-09 21:12:49 -0500

    Crea la primera versión de la pantalla de Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-      <h2>Esto es un texto de prueba para ver que no esté explotando</h2>
+    /*
+    <div className='back'>
+      <nav>
+        <div className="logo">
+            <img alt="logo" />
+        </div>
+        <ul className='nav-list'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Works</li>
+            <li>Contact</li>
+        </ul>
+        
+      </nav>
     </div>
+    */
+    <div className='back'>
+    <div id='up'></div>
+    <div id='down'></div>
+    <div id='left'></div>
+    <div id='right'></div>
+    <div className='back'>
+      <nav>
+        <div className="logo">
+            <img alt="logo" />
+        </div>
+        <ul className='nav-list'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Works</li>
+            <li>Contact</li>
+        </ul>
+        
+      </nav>
+    </div>
+    <div className='details'>
+      <div className='text'>
+          <h3 id='slogan'>JUAN ESTEBAN PINILLA AGUILERA</h3>
+          <br></br>
+          <br></br>
+          <p id='explicacion'>Full Stack Developer - Software Architect</p>
+          </div> 
+    </div>
+  </div>
+    // xdxdxd
+
+  
   );
 }
 
